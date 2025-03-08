831e0b6cc4a47a4b8a035c38185c07faaa2d0f45
dirhana <dirhana69@gmail.com>
2025-03-08 14:53:53 +0800

    web

---
+++ env.js
@@ -0,0 +1,27 @@
+window.settings = {
+  // 站点标题
+  title: '樱花云☁️',
+  // 站点描述
+  description: '高速 | 匿名 | 稳定 | 实惠 | 保障',
+  // API
+  host: 'https://1322253634-0oyi7dn0aa.ap-shanghai.tencentscf.com',
+  // 主题
+  theme: {
+    sidebar: 'dark',
+    header: 'dark',
+    color: 'darkblue'
+  },
+  // 背景 https://www.hostpic.org/images/2401032021260103.jpeg
+  background_url: 'https://img.meituan.net/video/dceb5e50e20ca54dd186dc82ecf2987d61135.jpg',
+  // crisp
+  crisp_id: '',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
